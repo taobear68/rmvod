f0abc20ccba69596a329e8a12bb7a2712e44eda6
Paul Tourville <tourvilp@vm-um1804-jobhunt-01.fairweather.ribbbitmedia.com>
2023-08-02 10:33:47 -0400

    Use API Config values Take 17

--- js/rmvod_wa_core.js
+++ js/rmvod_wa_core.js
@@ -1069,7 +1069,8 @@ class RMVodWebApp {
         }
         const payloadObj = {'table':'companies'};
         //const endpoint = '/rmvod/api/suplist/get';
-        var apiBase = this.sse.ssRead('apicfg')['API_Resources']['api_path'];
+        //var apiBase = this.sse.ssRead('apicfg')['API_Resources']['api_path'];
+        var apiBase = this.getApiConfigValue('API_Resources','api_path');
         const endpoint = apiBase + '/suplist/get';
         var result = this.genericApiCall(payloadObj,endpoint,cbFunc);
     }
@@ -1086,7 +1087,8 @@ class RMVodWebApp {
         }
         const payloadObj = {'table':'tags'};
         //const endpoint = '/rmvod/api/suplist/get';
-        var apiBase = this.sse.ssRead('apicfg')['API_Resources']['api_path'];
+        //var apiBase = this.sse.ssRead('apicfg')['API_Resources']['api_path'];
+        var apiBase = this.getApiConfigValue('API_Resources','api_path');
         const endpoint = apiBase + '/suplist/get';
         var result = this.genericApiCall(payloadObj,endpoint,cbFunc);
     }
