38c7c8db0bd02d1d4332668882b8affe3c5a6d71
Paul Tourville <tourvilp@vm-um1804-jobhunt-01.fairweather.ribbbitmedia.com>
2023-08-06 18:17:44 -0400

    caching recs Take 9

--- py/rmvod_api.py
+++ py/rmvod_api.py
@@ -1562,6 +1562,7 @@ ORDER BY 1"""
             retList.append(row[0])
         return retList
     def writeRecToCache(self,clientIdIn=None,recDictIn=None,expDurDaysIn=7):
+        print("writeRecToCache - clientIdIn: " + clientIdIn + ", expDurDaysIn: " + str(expDurDaysIn) + ", clientIdIn: " + json.dumps(clientIdIn))
         uRecId = str(uuid.uuid4())
         tmpSql = """INSERT INTO common_texts
 SET id = '""" + uRecId + """',
