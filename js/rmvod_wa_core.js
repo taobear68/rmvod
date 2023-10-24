e9d4bda4d4dcfcc726c67a11811e5851553fbab5
Paul Tourville <tourvilp@vm-um1804-jobhunt-01.fairweather.ribbbitmedia.com>
2023-10-24 07:35:36 -0400

    user session -- imtegrate UI prototype bits -- take 1

--- js/rmvod_wa_core.js
+++ js/rmvod_wa_core.js
@@ -343,9 +343,32 @@ class RMVWAHtmlGenerator {
         cell3Div.style.marginLeft = "10px";
         cell3Div.style.marginTop = "30px";
         cell3Div.id = "headerblock3";
-        var tmpHtml = '';
-        tmpHtml += "<b>0000-11-22 11:22</b>"
-        cell3Div.innerHTML = tmpHtml;
+        //var tmpHtml = '';
+        //tmpHtml += "<b>0000-11-22 11:22</b>"
+        //cell3Div.innerHTML = tmpHtml;
+        
+        var htmlStr = "";
+        htmlStr += '<div id="clockdisp" style="display:block; background-color:#bbbbbb; padding: 2px; ">2023-10-23 01:23:45</div>';
+        htmlStr += '<div id="sessiondisp"  style="display:block; background-color:#999999; padding: 2px; ">';
+        htmlStr += '<div id="sessiondata" style="display:none" data-session="{&quot;userid&quot;:&quot;NONE&quot;, &quot;userdetail&quot;:{},&quot;sessiondetails&quot;:{,&quot;sessionjson&quot;:{}}}"></div>';
+        htmlStr += '<div id="sessionno" style="display:block; background-color:#ffdddd;">';
+        htmlStr += '<span id="loginbutton" onclick="doLoginButton()"><b><u>Login</u></b></span>';
+        htmlStr += '</div>';
+        htmlStr += '<div id="sessionyes" style="display:none; background-color:#ddffdd;">';
+        htmlStr += '<i>Watching as</i><br>';
+        htmlStr += '<span id="sessionpersonname"></span><br>';
+        htmlStr += '(<span id="logoutbutton" onclick="doLogoutButton()"><u>Logout</u></span>)&nbsp;';
+        htmlStr += '(<span id="closesessbutton" onclick="doCloseSessButton()"><u>Close Session</u></span>)';
+        htmlStr += '</div>';
+        htmlStr += '<div id="sessionstart" style="display:none; background-color:#ddddff;">';
+        htmlStr += '<form id="loginform">';
+        htmlStr += 'Username:&nbsp;<input id="username" type="text" tabindex="1"><br>';
+        htmlStr += 'Password:&nbsp;<input id="password" type="password" tabindex="1"><br>';
+        htmlStr += '<span id="logingobutton" onclick="doLoginGoButton()" tabindex="3"><b><u>Login</u></b></span>';
+        htmlStr += '</form>';
+        htmlStr += '</div>';
+        htmlStr += '</div>';            
+        cell3Div.innerHTML = htmlStr;
         
         hContDiv.appendChild(cell1Div);
         hContDiv.appendChild(cell2Div);
@@ -1013,7 +1036,8 @@ class RMVodWebApp {
             var dateStr = yr.toString() + '-' + moStr + '-' + dyStr;
             var timeStr = hrStr + ':' + mnStr;
             var datTimStr = dateStr + ' ' + timeStr;
-            document.getElementById('headerblock3').innerHTML = '<div><b>' + datTimStr + '</b></div>';
+            //document.getElementById('headerblock3').innerHTML = '<div><b>' + datTimStr + '</b></div>';
+            document.getElementById('clockdisp').innerHTML = '<b>' + datTimStr + '</b>';
         }
         gtFunc();
         var intv = setInterval(gtFunc,15000);
@@ -3302,6 +3326,86 @@ class RMVodWebApp {
         const payload = {'artifactid':artiIdIn};
         this.genericApiCall(payload,apiEndpoint,cbFunc);
     }
+    
+    
+    //
+    //
+    // User Session bits -- BEGIN
+    //
+    //
+    
+    doLoginButton () {
+        document.getElementById('clockdisp').style.display = "none";
+        document.getElementById('sessionstart').style.display = "block";
+        document.getElementById('sessionno').style.display = "none";
+        document.getElementById('username').focus();
+        //document.getElementById('').style.display = "none";
+        //document.getElementById('').style.display = "none";
+        //document.getElementById('').style.display = "none";
+    }
+    doLoginGoButton () {
+        document.getElementById('sessionstart').style.display = "none";
+        document.getElementById('clockdisp').style.display = "block";
+        document.getElementById('sessionyes').style.display = "block";
+        var unm = document.getElementById('username').value;
+        document.getElementById('username').value = "";
+        var pw = document.getElementById('password').value
+        document.getElementById('password').value = "";
+        // Do the "start session" thing
+        var sessDetObj = {"userid":"12345678-9abc-defg-hijk-lmnopqrstuvw", "userdetail":{"loginname": "PaulTourville", "propername": "Paul Tourville", "metajson": {"dachshund": "silly", "listothings": ["person", "man", "womam", "camera", "tv"]}},"sessiondetails":{"sessiontoken": "9694eb13-6507-4ee3-8c1a-bc598ac31372", "sessionexpiredt": "2024-10-12 06:11:07","sessionjson":{"cookies":{}}}};
+        document.getElementById('sessionpersonname').innerHTML = "<b>" + sessDetObj['userdetail']['propername'] + "</b>";
+        document.getElementById('sessiondata').dataset.session = JSON.stringify(sessDetObj);
+    }
+    doLogoutButton () {
+        // Logout reverts this browser to "not logged in" status, reporting its own ID as the userid, and respecting local cookies
+        document.getElementById('sessionyes').style.display = "none";
+        document.getElementById('sessionno').style.display = "block";
+        
+        var sessDetObj = {"userid":"thisIsAFakeId-Netscape-1683026819380", "userdetail":{"loginname": "", "propername": "", "metajson": {"dachshund": "silly", "listothings": ["person", "man", "womam", "camera", "tv"]}},"sessiondetails":{"sessiontoken": "", "sessionexpiredt": "","sessionjson":{"cookies":{}}}};
+        document.getElementById('sessionpersonname').innerHTML = "<b>" + sessDetObj['userdetail']['propername'] + "</b>";
+        document.getElementById('sessiondata').dataset.session = JSON.stringify(sessDetObj);
+    }
+    doCloseSessButton() {
+        // Close Session closes the session on the server via API call, then runs doLogoutButton
+        doLogoutButton();
+    }
+    handleKeyPress(e) {
+        if (e.keycode === 13) {
+            doLoginGoButton();
+        }
+    }
+    renderHB3Content () {
+        var htmlStr = "";
+        htmlStr += '<div id="clockdisp" style="display:block; background-color:#bbbbbb; padding: 2px; ">2023-10-23 01:23:45</div>';
+        htmlStr += '<div id="sessiondisp"  style="display:block; background-color:#999999; padding: 2px; ">';
+        htmlStr += '<div id="sessiondata" style="display:none" data-session="{&quot;userid&quot;:&quot;NONE&quot;, &quot;userdetail&quot;:{},&quot;sessiondetails&quot;:{,&quot;sessionjson&quot;:{}}}"></div>';
+        htmlStr += '<div id="sessionno" style="display:block; background-color:#ffdddd;">';
+        htmlStr += '<span id="loginbutton" onclick="doLoginButton()"><b><u>Login</u></b></span>';
+        htmlStr += '</div>';
+        htmlStr += '<div id="sessionyes" style="display:none; background-color:#ddffdd;">';
+        htmlStr += '<i>Watching as</i><br>';
+        htmlStr += '<span id="sessionpersonname"></span><br>';
+        htmlStr += '(<span id="logoutbutton" onclick="doLogoutButton()"><u>Logout</u></span>)&nbsp;';
+        htmlStr += '(<span id="closesessbutton" onclick="doCloseSessButton()"><u>Close Session</u></span>)';
+        htmlStr += '</div>';
+        htmlStr += '<div id="sessionstart" style="display:none; background-color:#ddddff;">';
+        htmlStr += '<form id="loginform">';
+        htmlStr += 'Username:&nbsp;<input id="username" type="text" tabindex="1"><br>';
+        htmlStr += 'Password:&nbsp;<input id="password" type="password" tabindex="1"><br>';
+        htmlStr += '<span id="logingobutton" onclick="doLoginGoButton()" tabindex="3"><b><u>Login</u></b></span>';
+        htmlStr += '</form>';
+        htmlStr += '</div>';
+        htmlStr += '</div>';            
+        document.getElementById('headerblock3').innerHTML = htmlStr;
+    }        
+        
+    //
+    //
+    // User Session bits -- BEGIN
+    //
+    //
+    
+    
 }
 
 //
@@ -3818,6 +3922,82 @@ class WMCWARecommend {
 //
 // NEW CODE TO SUPPORT RECOMMENDATIONS -- END
 //
+
+
+// 
+// NEW CODE TO SUPPORT SESSIONS -- START
+//
+
+//function doLoginButton () {
+    //document.getElementById('clockdisp').style.display = "none";
+    //document.getElementById('sessionstart').style.display = "block";
+    //document.getElementById('sessionno').style.display = "none";
+    //document.getElementById('username').focus();
+    ////document.getElementById('').style.display = "none";
+    ////document.getElementById('').style.display = "none";
+    ////document.getElementById('').style.display = "none";
+//}
+//function doLoginGoButton () {
+    //document.getElementById('sessionstart').style.display = "none";
+    //document.getElementById('clockdisp').style.display = "block";
+    //document.getElementById('sessionyes').style.display = "block";
+    //var unm = document.getElementById('username').value;
+    //document.getElementById('username').value = "";
+    //var pw = document.getElementById('password').value
+    //document.getElementById('password').value = "";
+    //// Do the "start session" thing
+    //var sessDetObj = {"userid":"12345678-9abc-defg-hijk-lmnopqrstuvw", "userdetail":{"loginname": "PaulTourville", "propername": "Paul Tourville", "metajson": {"dachshund": "silly", "listothings": ["person", "man", "womam", "camera", "tv"]}},"sessiondetails":{"sessiontoken": "9694eb13-6507-4ee3-8c1a-bc598ac31372", "sessionexpiredt": "2024-10-12 06:11:07","sessionjson":{"cookies":{}}}};
+    //document.getElementById('sessionpersonname').innerHTML = "<b>" + sessDetObj['userdetail']['propername'] + "</b>";
+    //document.getElementById('sessiondata').dataset.session = JSON.stringify(sessDetObj);
+//}
+//function doLogoutButton () {
+    //// Logout reverts this browser to "not logged in" status, reporting its own ID as the userid, and respecting local cookies
+    //document.getElementById('sessionyes').style.display = "none";
+    //document.getElementById('sessionno').style.display = "block";
+    
+    //var sessDetObj = {"userid":"thisIsAFakeId-Netscape-1683026819380", "userdetail":{"loginname": "", "propername": "", "metajson": {"dachshund": "silly", "listothings": ["person", "man", "womam", "camera", "tv"]}},"sessiondetails":{"sessiontoken": "", "sessionexpiredt": "","sessionjson":{"cookies":{}}}};
+    //document.getElementById('sessionpersonname').innerHTML = "<b>" + sessDetObj['userdetail']['propername'] + "</b>";
+    //document.getElementById('sessiondata').dataset.session = JSON.stringify(sessDetObj);
+//}
+//function doCloseSessButton() {
+    //// Close Session closes the session on the server via API call, then runs doLogoutButton
+    //doLogoutButton();
+//}
+//function handleKeyPress(e) {
+    //if (e.keycode === 13) {
+        //doLoginGoButton();
+    //}
+//}
+//function renderHB3Content () {
+    //var htmlStr = "";
+    //htmlStr += '<div id="clockdisp" style="display:block; background-color:#bbbbbb; padding: 2px; ">2023-10-23 01:23:45</div>';
+    //htmlStr += '<div id="sessiondisp"  style="display:block; background-color:#999999; padding: 2px; ">';
+    //htmlStr += '<div id="sessiondata" style="display:none" data-session="{&quot;userid&quot;:&quot;NONE&quot;, &quot;userdetail&quot;:{},&quot;sessiondetails&quot;:{,&quot;sessionjson&quot;:{}}}"></div>';
+    //htmlStr += '<div id="sessionno" style="display:block; background-color:#ffdddd;">';
+    //htmlStr += '<span id="loginbutton" onclick="doLoginButton()"><b><u>Login</u></b></span>';
+    //htmlStr += '</div>';
+    //htmlStr += '<div id="sessionyes" style="display:none; background-color:#ddffdd;">';
+    //htmlStr += '<i>Watching as</i><br>';
+    //htmlStr += '<span id="sessionpersonname"></span><br>';
+    //htmlStr += '(<span id="logoutbutton" onclick="doLogoutButton()"><u>Logout</u></span>)&nbsp;';
+    //htmlStr += '(<span id="closesessbutton" onclick="doCloseSessButton()"><u>Close Session</u></span>)';
+    //htmlStr += '</div>';
+    //htmlStr += '<div id="sessionstart" style="display:none; background-color:#ddddff;">';
+    //htmlStr += '<form id="loginform">';
+    //htmlStr += 'Username:&nbsp;<input id="username" type="text" tabindex="1"><br>';
+    //htmlStr += 'Password:&nbsp;<input id="password" type="password" tabindex="1"><br>';
+    //htmlStr += '<span id="logingobutton" onclick="doLoginGoButton()" tabindex="3"><b><u>Login</u></b></span>';
+    //htmlStr += '</form>';
+    //htmlStr += '</div>';
+    //htmlStr += '</div>';            
+    //document.getElementById('headerblock3').innerHTML = htmlStr;
+//}        
+
+// 
+// NEW CODE TO SUPPORT SESSIONS -- END
+//
+
+
 
 // Provide a single entrypoint into the RMVodWebApp
 function switchboard(actionIn,objIdIn,argObjIn) {
