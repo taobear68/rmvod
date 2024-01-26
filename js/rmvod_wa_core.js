95413df2b75fbd436e459be86f2484482d9db357
Paul Tourville <tourvilp@vm-um1804-jobhunt-01.fairweather.ribbbitmedia.com>
2024-01-26 12:51:44 -0500

    Recent Episodes        TAKE 36

--- js/rmvod_wa_core.js
+++ js/rmvod_wa_core.js
@@ -1185,7 +1185,8 @@ class RMVodWebApp {
                 var cellDiv1 = document.createElement('div');
                 cellDiv1.style.display = "inline-flex";
                 cellDiv1.style.width = colWidthPct[0];
-                cellDiv1.innerText = objIn['data'][row]['seriestitle']
+                // cellDiv1.innerText = objIn['data'][row]['seriestitle']
+                cellDiv1.innerHTML = "<span onclick=\"switchboard('statpopsidelist','" + objIn['data'][row]['serieseartifactid'] + "',{})\"><u>" + objIn['data'][row]['seriestitle'] + '</u></span>';
                 rowDiv.appendChild(cellDiv1);
                 // Episode Title
                 var cellDiv2 = document.createElement('div');
