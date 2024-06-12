19ed10cd0adc358e3b14f6b7c4900675eca27796
Paul Tourville (primitive) <taobear68@gmail.com>
2024-06-12 05:06:38 -0400

    correcting error in 'server' rec queries.

--- py/rmvod_api.py
+++ py/rmvod_api.py
@@ -2102,14 +2102,14 @@ GROUP BY 1, 2, 3 """
             sql_server_tv = """SELECT DISTINCT  '""" + userId + """' AS "clientid", a.artifactid, a.title, a.majtype, a.imdbid 
     FROM artifacts a 
     WHERE a.majtype = "tvseries" 
-      AND a.artifactid NOT IN ( SELECT artifactid FROM rec_logged_plays ) 
+      AND a.artifactid NOT IN ( SELECT seriesaid  FROM rec_logged_plays WHERE majtype = 'tvepisode'  ) 
     LIMIT """ + str(limitIn) + """ """
             sqlObj['server']['tvseries'] = sql_server_tv
             #   Fetch "server" Recs for movies 
             sql_server_movie = """SELECT DISTINCT  '""" + userId + """' AS "clientid", a.artifactid, a.title, a.majtype, a.imdbid 
     FROM artifacts a 
     WHERE a.majtype = "movie" 
-      AND a.artifactid NOT IN ( SELECT seriesaid FROM rec_logged_plays WHERE majtype = "tvepisode" )
+      AND a.artifactid NOT IN ( SELECT seriesaid FROM rec_logged_plays )
     LIMIT """ + str(limitIn) + """ """
             sqlObj['server']['movie'] = sql_server_movie
             for recType in list(sqlObj.keys()):
