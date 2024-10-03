c88c0ec731d9a442962e0ad2fd646ca486c019ec
Paul Tourville (primitive) <taobear68@gmail.com>
2024-10-03 12:04:01 -0400

    integrating playlist edit from POC  - TAKE 9

--- js/rmvod_wa_core.js
+++ js/rmvod_wa_core.js
@@ -4377,7 +4377,7 @@ class PLHander {
     pleGetPlayistToEdit(tvsplIdIn){
         var plOmniObj = JSON.stringify(document.getElementById('plhandlerdata').dataset.omniobj);
         var plList = plOmniObj['pl-list'];
-        console.log('PLHander.pleGetPlayistToEdit - plList: ' + JSON.strignify(plList));
+        console.log('PLHander.pleGetPlayistToEdit - plList: ' + JSON.stringify(plList));
         return plList[tvsplIdIn];
     }
     //plhandlerdata data-omniobj
