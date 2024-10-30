f6c911e086b54d28fd65bb74fcac19581f1a27cd
Paul Tourville (primitive) <taobear68@gmail.com>
2024-10-30 07:26:04 -0400

    Add Replay and PlayNext to Recs Arti Detail for TV Series  TAKE 5

--- js/rmvod_wa_core.js
+++ js/rmvod_wa_core.js
@@ -2,7 +2,7 @@
 //rmvod_wa_core.js  Copyright 2022, 2023, 2024 Paul Tourville
 
 //This file is part of RIBBBITmedia VideoOnDemand (a.k.a. "rmvod").
-\
+
 //RIBBBITmedia VideoOnDemand (a.k.a. "rmvod") is free software: you 
 //can redistribute it and/or modify it under the terms of the GNU 
 //General Public License as published by the Free Software 
